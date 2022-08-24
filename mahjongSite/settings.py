3c25ac611f70101b313397185e297975371b7de4
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-08-25 02:06:08 +0900

    Change DB from sqlite to postgreSQL

--- mahjongSite/settings.py
+++ mahjongSite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -82,9 +83,17 @@ WSGI_APPLICATION = 'mahjongSite.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+         # 'ENGINE': 'django.db.backends.sqlite3',
+         # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nnkrdb',
+        'USER': 'admin',
+        'PASSWORD': 'ramanujan',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
