c00cbbbf9c21228244772979e678934d47fc73d0
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-09-03 08:56:44 +0900

    adjust icon model

--- user/models.py
+++ user/models.py
@@ -12,13 +12,27 @@ class Icon(models.Model):
     def url(self):
         return static("img/icon/" + self.name)
 
-def get_or_create_guest_icon():
-    icon, _ = Icon.objects.get_or_create(name='icon_guest.png')
-    return icon
+    @classmethod
+    def get_default(cls):
+        icon, _ = Icon.objects.get_or_create(name='icon_guest.png')
+        return icon
 
+    @classmethod
+    def get_default_pk(cls):
+        icon = cls.get_default()
+        return icon.pk
+
+
+"""
+iconのdefaultにIconを使っているためマイグレーション時にエラー発生
+django.db.utils.ProgrammingError: relation does not exist
+一度iconのderaultを外しmigrateしてからだと上手くいく。
+"""
 class User(AbstractUser):
     introduction = models.TextField('紹介文',blank=True)
-    icon = models.ForeignKey(Icon,on_delete=models.SET_DEFAULT,blank=True,null=True,verbose_name="アイコン",default=get_or_create_guest_icon)
+    icon = models.ForeignKey(Icon,blank=True,null=True,verbose_name="アイコン",
+        on_delete=models.SET(Icon.get_default),
+        default=Icon.get_default_pk)
     #icon = models.ForeignKey(Icon,on_delete=models.PROTECT,blank=True,null=True,verbose_name="アイコン")
     
 
