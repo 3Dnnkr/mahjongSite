cddcedde32fce5677435aedb2f1e4e5281b8819b
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-09-13 23:37:50 +0900

    arange ga4

--- ga4/ga4_api.py
+++ ga4/ga4_api.py
@@ -3,12 +3,15 @@ from google.analytics.data_v1beta.types import Dimension
 from google.analytics.data_v1beta.types import Metric
 from google.analytics.data_v1beta.types import RunRealtimeReportRequest
 import os
+
+from mahjongSite.settings import BASE_DIR
 from django.conf import settings
 from django.templatetags.static import static
 
 
 def get_active_user_num():
     os.environ.setdefault('GOOGLE_APPLICATION_CREDENTIALS', os.path.join(settings.BASE_DIR, static('json/client_secrets.json')))
+    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = BASE_DIR + static('json/client_secrets.json')
     #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = settings.BASE_DIR + static('json/client_secrets.json')
     # property_id = "331574379"
     # client = BetaAnalyticsDataClient()
