c70f1a856524224bd75cb4eb4c07245b9e6490ea
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-09-13 21:35:25 +0900

    arange ga4

--- mahjongSite/settings.py
+++ mahjongSite/settings.py
@@ -77,7 +77,7 @@ TEMPLATES = [
                 'social_django.context_processors.backends', 
                 'social_django.context_processors.login_redirect',
                 'mahjongSite.context_processors.google_analytics',
-                #'mahjongSite.context_processors.active_user_num',
+                'mahjongSite.context_processors.active_user_num',
             ],
             'builtins':[
                 'nnkr.templatetags.extra_tags'
