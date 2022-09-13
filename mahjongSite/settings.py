8491b04f403a4d902a5147345c93b9eb22ad96a9
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-09-13 21:48:37 +0900

    arange ga4

--- mahjongSite/settings.py
+++ mahjongSite/settings.py
@@ -14,7 +14,6 @@ from pathlib import Path
 import os
 import cloudinary
 from django.contrib import messages
-from django.templatetags.static import static
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -214,4 +213,3 @@ MESSAGE_TAGS = {
 # Google Analytics
 GOOGLE_ANALYTICS_TRACKING_ID ='G-JZRES8R7S7'
 #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = BASE_DIR + '/ga4/client_secrets.json'
-os.environ.setdefault('GOOGLE_APPLICATION_CREDENTIALS', BASE_DIR + static('json/client_secrets.json'))
