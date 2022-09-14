eec74d3326803e2408a8c26e43ce459cd9ae347a
aoisingo <blue_bird_1729@yahoo.co.jp>
2022-09-14 11:04:24 +0900

    arange ga4

--- ga4/ga4_api.py
+++ ga4/ga4_api.py
@@ -10,21 +10,20 @@ from django.templatetags.static import static
 
 
 def get_active_user_num():
-    os.environ.setdefault('GOOGLE_APPLICATION_CREDENTIALS', os.path.join(settings.BASE_DIR, static('json/client_secrets.json')))
-    os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = BASE_DIR + static('json/client_secrets.json')
-    #os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = settings.BASE_DIR + static('json/client_secrets.json')
-    # property_id = "331574379"
-    # client = BetaAnalyticsDataClient()
+    # os.environ.setdefault('GOOGLE_APPLICATION_CREDENTIALS', os.path.join(settings.BASE_DIR, static('json/client_secrets.json')))
+    # os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = BASE_DIR + static('json/client_secrets.json')
+    
+    property_id = "331574379"
+    client = BetaAnalyticsDataClient()
 
-    # request = RunRealtimeReportRequest(
-    #     property=f"properties/{property_id}",
-    #     metrics=[Metric(name="activeUsers")],
-    # )
-    # response = client.run_realtime_report(request)
-    # return response
-    #return sum([v for row in response.rows for v in row.metric_values])
+    request = RunRealtimeReportRequest(
+        property=f"properties/{property_id}",
+        metrics=[Metric(name="activeUsers")],
+    )
+    response = client.run_realtime_report(request)
+    return response
 
-    return os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    #return os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
 
 
 
